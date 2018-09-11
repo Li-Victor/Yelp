682890f43d52521aef6e81a6242d2d10a40c6c22
Victor Li <victorli5674@yahoo.com>
2018-09-11 10:33:42 -0700

    embedding in navigation and rounding corners for thumbnail image

--- Yelp/Views/BusinessCell.swift
+++ Yelp/Views/BusinessCell.swift
@@ -34,6 +34,8 @@ class BusinessCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        thumbImageView.layer.cornerRadius = 3
+        thumbImageView.clipsToBounds = true
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
