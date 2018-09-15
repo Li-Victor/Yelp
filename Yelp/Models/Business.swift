47b612720fd4e78bdfc9957b01ac733acdab5469
Victor Li <victorli5674@yahoo.com>
2018-09-14 22:18:42 -0700

    fix warning constraints and putting into logic in the Business class

--- Yelp/Models/Business.swift
+++ Yelp/Models/Business.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SwiftyJSON
 
 struct Business {
     let name: String
@@ -17,4 +18,59 @@ struct Business {
     let distance: String
     let ratingImage: UIImage
     let reviewCount: Int
+    
+    static func parseToBusiness(json: JSON) -> Business {
+        let name = json["name"].stringValue
+        let imageURLString = json["image_url"].stringValue
+        let imageURL = URL(string: imageURLString)!
+        
+        var address = ""
+        let addressArray = json["location", "display_address"].arrayValue
+        if addressArray.count > 0 {
+            address = addressArray[0].stringValue
+        }
+        if addressArray.count > 1 {
+            address += ", " + addressArray[1].stringValue
+        }
+        
+        let categoriesArray = json["categories"].arrayValue
+        var categoryNames = [String]()
+        for category in categoriesArray {
+            let categoryName = category["title"].stringValue
+            categoryNames.append(categoryName)
+        }
+        let categories = categoryNames.joined(separator: ", ")
+        
+        let distanceMeters = json["distance"].doubleValue
+        let milesPerMeter = 0.000621371
+        let distance = String(format: "%.2f mi", milesPerMeter * distanceMeters)
+        
+        let rating = json["rating"].doubleValue
+        let ratingImage: UIImage
+        switch rating {
+        case 1:
+            ratingImage = UIImage(named: "stars_1")!
+        case 1.5:
+            ratingImage = UIImage(named: "stars_1_half")!
+        case 2:
+            ratingImage = UIImage(named: "stars_2")!
+        case 2.5:
+            ratingImage = UIImage(named: "stars_2_half")!
+        case 3:
+            ratingImage = UIImage(named: "stars_3")!
+        case 3.5:
+            ratingImage = UIImage(named: "stars_3_half")!
+        case 4:
+            ratingImage = UIImage(named: "stars_4")!
+        case 4.5:
+            ratingImage = UIImage(named: "stars_4_half")!
+        case 5:
+            ratingImage = UIImage(named: "stars_5")!
+        default:
+            ratingImage = UIImage(named: "stars_0")!
+        }
+        
+        let reviewCount = json["review_count"].intValue
+        return Business(name: name, address: address, imageURL: imageURL, categories: categories, distance: distance, ratingImage: ratingImage, reviewCount: reviewCount)
+    }
 }
--- Yelp/View Controllers/BusinessesViewController.swift
+++ Yelp/View Controllers/BusinessesViewController.swift
@@ -30,61 +30,6 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
     var longitude: CLLocationDegrees!
     var first = true
     
-    func parseToBusiness(json: JSON) -> Business {
-        let name = json["name"].stringValue
-        let imageURLString = json["image_url"].stringValue
-        let imageURL = URL(string: imageURLString)!
-        
-        var address = ""
-        let addressArray = json["location", "display_address"].arrayValue
-        if addressArray.count > 0 {
-            address = addressArray[0].stringValue
-        }
-        if addressArray.count > 1 {
-            address += ", " + addressArray[1].stringValue
-        }
-        
-        let categoriesArray = json["categories"].arrayValue
-        var categoryNames = [String]()
-        for category in categoriesArray {
-            let categoryName = category["title"].stringValue
-            categoryNames.append(categoryName)
-        }
-        let categories = categoryNames.joined(separator: ", ")
-        
-        let distanceMeters = json["distance"].doubleValue
-        let milesPerMeter = 0.000621371
-        let distance = String(format: "%.2f mi", milesPerMeter * distanceMeters)
-        
-        let rating = json["rating"].doubleValue
-        let ratingImage: UIImage
-        switch rating {
-        case 1:
-            ratingImage = UIImage(named: "stars_1")!
-        case 1.5:
-            ratingImage = UIImage(named: "stars_1_half")!
-        case 2:
-            ratingImage = UIImage(named: "stars_2")!
-        case 2.5:
-            ratingImage = UIImage(named: "stars_2_half")!
-        case 3:
-            ratingImage = UIImage(named: "stars_3")!
-        case 3.5:
-            ratingImage = UIImage(named: "stars_3_half")!
-        case 4:
-            ratingImage = UIImage(named: "stars_4")!
-        case 4.5:
-            ratingImage = UIImage(named: "stars_4_half")!
-        case 5:
-            ratingImage = UIImage(named: "stars_5")!
-        default:
-            ratingImage = UIImage(named: "stars_0")!
-        }
-        
-        let reviewCount = json["review_count"].intValue
-        return Business(name: name, address: address, imageURL: imageURL, categories: categories, distance: distance, ratingImage: ratingImage, reviewCount: reviewCount)
-    }
-    
 
     func fetchBusinesses(for term: String = "", offset: Int = 0) {
         guard let latitude = latitude, let longitude = longitude else {
@@ -106,7 +51,7 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
             if let error = error {
                 print(error.localizedDescription)
             } else if let data = data {
-                self.businesses += JSON(data)["businesses"].arrayValue.map { self.parseToBusiness(json: $0) }
+                self.businesses += JSON(data)["businesses"].arrayValue.map { Business.parseToBusiness(json: $0) }
             }
             
         }
